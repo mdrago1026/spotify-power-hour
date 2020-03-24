3b16211b488a3882f235455f3e7e52ad7225a4b3
Michael Drago <mdrago1026@gmail.com>
2020-03-24 11:14:59 -0400

    - wrote generic recursive pagination func - wrote func to get all songs on a given playlist id

--- src/spotify_client/spotify/api.clj
+++ src/spotify_client/spotify/api.clj
@@ -11,7 +11,8 @@
 
 (def urls
   {:token "https://accounts.spotify.com/api/token"
-   :playlist-get "https://api.spotify.com/v1/users/%s/playlists?offset=0&limit=20"
+   :playlists-get "https://api.spotify.com/v1/users/%s/playlists?offset=0&limit=20"
+   :playlists-list-songs "https://api.spotify.com/v1/playlists/%s/tracks"
    :oauth-url "https://accounts.spotify.com/authorize?client_id=ff1826b82af24af9b95d0a951a676ab5&response_type=code&redirect_uri=https%3A%2F%2Fhaloof-dev.ngrok.io%2Fspotify%2Fcallback&scope=user-read-private%20user-read-email%20playlist-read-collaborative"})
 
 (defn call-oauth-url []
@@ -35,45 +36,72 @@
 ;;(get-access-token)
 
 (defn get-user-playlists* [user & {:keys [url]}]
-  (let [url (or url (format (:playlist-get urls) (or user spotify-user)))
+  (let [url (or url (format (:playlists-get urls) (or user spotify-user)))
         {:keys [status body headers] :as resp}
         (client/get url {:headers {"Authorization" (str "Bearer " (:access_token @cmn-session/spotify-session))
                                    "Content-type" "application/json; charset=utf-8"}})
         parsed-body (json/parse-string body true)]
     parsed-body))
 
-(defn get-user-playlists [user]
-  (let [all-paged-data (loop [url nil
-                              overall-playlists []]
-                         (let [{:keys [next] :as curr-playlists} (get-user-playlists* user :url url)]
-                           (if next
-                             (recur
-                               next
-                               (conj overall-playlists curr-playlists))
-                             (conj overall-playlists curr-playlists))))
+;(defn get-user-playlists [user]
+;  (let [all-paged-data (loop [url nil
+;                              overall-playlists []]
+;                         (let [{:keys [next] :as curr-playlists} (get-user-playlists* user :url url)]
+;                           (if next
+;                             (recur
+;                               next
+;                               (conj overall-playlists curr-playlists))
+;                             (conj overall-playlists curr-playlists))))
+;        combined-data (reduce
+;                        (fn [r {:keys [items]}]
+;                          (concat r items)) [] all-paged-data)]
+;    combined-data))
+
+(defn handle-paginated-requests [initial-url]
+  (let [all-paged-data
+        (loop [url nil
+               overall-data []]
+          (let [{:keys [status body headers] :as resp}
+                (client/get (or url initial-url)
+                            {:headers {"Authorization" (str "Bearer " (:access_token @cmn-session/spotify-session))
+                                       "Content-type" "application/json; charset=utf-8"}})
+                {:keys [next] :as parsed-body} (json/parse-string body true)]
+            (if next
+              (recur
+                next
+                (conj overall-data parsed-body))
+              (conj overall-data parsed-body))))
         combined-data (reduce
                         (fn [r {:keys [items]}]
                           (concat r items)) [] all-paged-data)]
     combined-data))
 
-(defn filter-playlist-by-name [search-term playlists-obj]
-  (let [{:keys [href items] :as playlists-obj} playlists-obj
-        filtered (filterv (fn [{:keys [name]}] (= (s/lower-case name) (s/lower-case search-term))) items)]
-    (assoc playlists-obj :items filtered)))
+(defn filter-playlist-by-name [search-term playlists-vec]
+  (let [filtered (filterv (fn [{:keys [name]}] (= (s/lower-case name) (s/lower-case search-term))) playlists-vec)]
+    filtered))
 
-;(mapv :name (:items (get-user-playlists "mdrago1026")))
-;;;
-;(filter-playlist-by-name "HaLo NIGHT" (get-user-playlists "mdrago1026"))
-;
-;;; left off: why are some of my playlists missing?
-;
-;
 ;;; https://open.spotify.com/playlist/75M2u29GVTzqp5q6p51IRC?si=NQRCUMapQ-S37eMg_PhOYQ
 ;
 ;;; https://api.spotify.com/v1/users/mdrago1026/playlists/75M2u29GVTzqp5q6p51IRC?si=NQRCUMapQ-S37eMg_PhOYQ/tracks
-;
-;
-;(get-user-playlists* "mdrago1026" :url "https://api.spotify.com/v1/users/mdrago1026/playlists?offset=20&limit=20#")
-;
-;(def recur-future (future (get-user-playlists "mdrago1026")))
+
+(defn plalyist-id->track-list [playlist-id]
+  (let [url (format (:playlists-list-songs urls) playlist-id)
+        {:keys [status body headers] :as resp}
+        (client/get url {:headers {"Authorization" (str "Bearer " (:access_token @cmn-session/spotify-session))
+                                   "Content-type" "application/json; charset=utf-8"}})
+        parsed-body (json/parse-string body true)]
+    parsed-body))
+
+
+(def my-user-playlists (handle-paginated-requests (format (:playlists-get urls) "mdrago1026")))
+
+(def halo-night-playlist-id (-> (filter-playlist-by-name "HALO nighT" my-user-playlists) first :id))
+(def halo-night-songs (plalyist-id->track-list halo-night-playlist-id))
+
+
+(def halo-night-songs
+  (handle-paginated-requests (format (:playlists-list-songs urls) halo-night-playlist-id)))
+
+;;(count halo-night-songs)
+
 
